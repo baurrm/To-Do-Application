e77ee608d2cfafa1eb3208c7423ea9771f07dc88
baurm12 <59176926+baurm12@users.noreply.github.com>
2021-06-01 14:36:12 +0200

    Project files

---
+++ ToDoApp/MyToDoList/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Bauyrzhan Marat on 21.04.2021.
+import RealmSwift
+import UIKit
+
+/*
+ - To show list of current to do list itmes
+ - To enter new to do list items
+ - To show previously entered to do list item
+
+ - Item
+ - Date
+ */
+
+class ToDoListItem: Object {
+    @objc dynamic var item: String = ""
+    @objc dynamic var date: Date = Date()
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var table: UITableView!
+    @IBOutlet var emptyItemView: UIView!
+
+    private let realm = try! Realm()
+    private var data = [ToDoListItem]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        data = realm.objects(ToDoListItem.self).map({ $0 })
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.delegate = self
+        table.dataSource = self
+    }
+     
+    // Prepare the empty view
+    
+     func numberOfSections(in tableView: UITableView) -> Int {
+        tableView.backgroundView = emptyItemView
+        tableView.backgroundView?.isHidden = true
+        if data.count > 0 {
+            tableView.backgroundView?.isHidden = true
+            tableView.separatorStyle = .singleLine
+        } else {
+            tableView.backgroundView?.isHidden = false
+            tableView.separatorStyle = .none
+        }
+    return 1
+        
+     }
+     
+
+    // Mark: Table
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row].item
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        // Open the screen where we can see item info and dleete
+        let item = data[indexPath.row]
+
+        guard let vc = storyboard?.instantiateViewController(identifier: "view") as? ViewViewController else {
+            return
+        }
+
+        vc.item = item
+        vc.deletionHandler = { [weak self] in
+            self?.refresh()
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.title = item.item
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+    @IBAction func didTapAddButton() {
+        guard let vc = storyboard?.instantiateViewController(identifier: "enter") as? EntryViewController else {
+            return
+        }
+        vc.completionHandler = { [weak self] in
+            self?.refresh()
+        }
+        vc.title = "New Item"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+    func refresh() {
+        data = realm.objects(ToDoListItem.self).map({ $0 })
+        table.reloadData()
+    }
+
+}
+
